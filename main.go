9329887c77b8d0e1cb4e5f508e20ac3e5ce3cf77
elzup <guild0105@gmail.com>
2017-05-18 10:17:12 +0900

    Add schemas

--- main.go
+++ main.go
@@ -19,17 +19,39 @@ type (
 		Token string
 	}
 
+	// UserPost : User request format
 	UserPost struct {
 		Name string `json:"name"`
 		Pass string `json:"pass"`
 	}
+
+	// AccessPoint : Wifi AccessPoint profiles
+	AccessPoint struct {
+		Ssid  string
+		Bssid string
+	}
+
+	// Follow : User and AccessPoint follow relation
+	Follow struct {
+		User        User
+		AccessPoint AccessPoint
+	}
+
+	// Checkin : User checkins
+	Checkin struct {
+		User        User
+		AccessPoint AccessPoint
+		ts          dbr.NullTime
+	}
 )
 
 var (
-	tablename = "User"
-	seq       = 1
-	conn, _   = dbr.Open("mysql", os.Getenv("MYSQL_URL"), nil)
-	sess      = conn.NewSession(nil)
+	usersTable        = "users"
+	checkinsTable     = "logs"
+	accessPointsTable = "access_points"
+	seq               = 1
+	conn, _           = dbr.Open("mysql", os.Getenv("MYSQL_URL"), nil)
+	sess              = conn.NewSession(nil)
 )
 
 func randToken() string {
@@ -38,21 +60,41 @@ func randToken() string {
 	return fmt.Sprintf("%x", b)
 }
 
+func createUser(c echo.Context) error {
+	u := new(UserPost)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	token := randToken()
+	fmt.Println(u)
+	fmt.Println(u.Name)
+	fmt.Println(u.Pass)
+	result, err := sess.
+		InsertInto(usersTable).
+		Columns("name", "pass", "token").
+		Values(u.Name, u.Pass, token).Exec()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
+	response := map[string]string{"token": token}
+	return c.JSON(http.StatusCreated, response)
+}
+
+func selectLogs(c echo.Context) error {
+	u := new(UserPost)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	token := randToken()
+	sess.InsertInto(usersTable).Columns("name", "pass", "token").Values(u.Name, u.Pass, token).Exec()
+	return c.JSON(http.StatusCreated, token)
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
 
-	e.POST("/users", func(c echo.Context) error {
-		u := new(UserPost)
-		if err := c.Bind(u); err != nil {
-			return err
-		}
-		token := randToken()
-		sess.InsertInto(tablename).Columns("name", "pass", "token").Values(u.Name, u.Pass, token).Exec()
-		return c.JSON(http.StatusCreated, token)
-	})
+	e.POST("/users", createUser)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
